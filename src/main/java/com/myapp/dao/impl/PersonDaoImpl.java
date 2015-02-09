6c9ea742e84a2eb40dda0a425ac0aff67b6864bc
ragham <ramose@mail.ru>
2015-02-09 23:10:14 +0300

    test commit

--- src/main/java/com/myapp/dao/impl/PersonDaoImpl.java
+++ src/main/java/com/myapp/dao/impl/PersonDaoImpl.java
@@ -12,7 +12,7 @@ public class PersonDaoImpl extends AbstractHibernateDAO<Person, Long> implements
 	@Override
 	public boolean isExist(Person entity)
 	{
-		
+//test
 //		return !findByCriteria(Restrictions.eq("name", entity.getName())).isEmpty();
 		return false;
 	}
