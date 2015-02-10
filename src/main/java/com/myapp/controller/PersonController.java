99b35a0496a8ac943cfc6e5f541d3f52bf235786
Ilya Ten <iten@alvioneurope.ru>
2015-02-10 23:04:20 +0300

    A service has been implemented as an additional layer

--- src/main/java/com/myapp/controller/PersonController.java
+++ src/main/java/com/myapp/controller/PersonController.java
@@ -1,21 +1,17 @@
 package com.myapp.controller;
 
-import java.util.List;
-
 import com.myapp.dao.api.IPersonDAO;
 import com.myapp.model.Person;
+import com.myapp.service.PersonService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.ClassUtils;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/person/")
@@ -27,13 +23,15 @@ public class PersonController
 
 	@Autowired
 	private IPersonDAO personDao;
+	@Autowired
+	private PersonService personService;
 
 	@RequestMapping(method = RequestMethod.GET, value = "list")
 	public ModelAndView listPeople()
 	{
 		logger.debug("Received request to list persons");
 		ModelAndView mav = new ModelAndView();
-		List<Person> people = personDao.findAll();
+		List<Person> people = personService.people();
 		logger.debug("Person Listing count = " + people.size());
 		mav.addObject("people", people);
 		mav.setViewName("list");
@@ -46,7 +44,7 @@ public class PersonController
 	{
 		logger.debug("Received request to add new person ");
 		ModelAndView mav = new ModelAndView();
-		Person person = new Person();
+		Person person = personService.getPerson(-1);
 		mav.addObject("person", person);
 
 		mav.setViewName("add");
@@ -59,7 +57,7 @@ public class PersonController
 	{
 		logger.debug("Received request to edit person id : " + id);
 		ModelAndView mav = new ModelAndView();
-		Person person = personDao.getById(id);
+		Person person = personService.getPerson(id);
 		mav.addObject("person", person);
 
 		mav.setViewName("edit");
@@ -71,7 +69,7 @@ public class PersonController
 	public String savePerson(@ModelAttribute("person") Person person)
 	{
 		logger.debug("Received postback on person " + person);
-		personDao.saveOrUpdate(person);
+		personService.savePerson(person);
 		return "redirect:list";
 	}
 
---
+++ src/main/java/com/myapp/service/PersonService.java
@@ -0,0 +1,16 @@
+package com.myapp.service;
+
+import com.myapp.model.Person;
+
+import java.util.List;
+
+/**
+ * Created by Ilya Ten
+ * Date: 2/9/2015
+ */
+public interface PersonService {
+    public List<Person> people();
+    public Person getPerson(long id);
+    public void savePerson(Person person);
+
+}
